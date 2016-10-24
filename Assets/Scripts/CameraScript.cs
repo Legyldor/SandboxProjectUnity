ce6f144b95c197e09f6a7e554b536b23c3e329aa
Joffrey Davaïc <joffrey.davaic@gmail.com>
2016-10-23 22:05:45 -0400

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	    camera = this.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ( isInputUnZoom() && this.camera.orthographicSize < 10)
+        {
+            unZoom();
+        }
+        else if (isInputZoom() && this.camera.orthographicSize > 1) 
+        {
+            zoom();
+        }
+	}
+
+    public bool isInputZoom()
+    {
+        bool result = false;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            result = true;
+        }
+        return result;
+    }
+
+    public bool isInputUnZoom()
+    {
+        bool result = false;
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            result = true;
+        }
+        return result;
+    } 
+
+    public void zoom()
+    {
+        this.camera.orthographicSize--;
+    }
+
+    public void unZoom()
+    {
+        this.camera.orthographicSize++;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    public string myName;
+    public Boolean isFront;
+    public Animator anim;
+    int standBackHash = Animator.StringToHash("Stand_Back");
+    int standFrontHash = Animator.StringToHash("Stand_Front");
+    public Transform transform;
+    public float speed = 50.0F;
+    public bool canMove = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("I am alive and my name is " + myName);
+        anim = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove){
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += move * speed;
+        }
+        
+
+        /*if (move.x > 0)
+        {
+             anim.SetTrigger(standFrontHash);
+         }
+        else if (move.x < 0)
+         {
+             anim.SetTrigger(standBackHash);
+         }*/
+    }
+
+    /*void OnCollisionEnter2D(Collision2D coll)
+    {
+        canMove = false;
+    }
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        canMove = true;
+
+    }*/
+}
