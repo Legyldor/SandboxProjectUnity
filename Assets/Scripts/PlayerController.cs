7c7d945b43ebe390ccd24638677432c32ac5d0d7
Joffrey Davaïc <joffrey.davaic@gmail.com>
2016-10-24 22:20:58 -0400

    edit
    Modify anim of a sprite and use of Time.deltatime (speed was always 1f
    cause it cant be instance in global declaration)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,11 @@ public class PlayerController : MonoBehaviour
     public string myName;
     public Boolean isFront;
     public Animator anim;
-    int standBackHash = Animator.StringToHash("Stand_Back");
-    int standFrontHash = Animator.StringToHash("Stand_Front");
+    public float speed;
+    public Rigidbody2D rigidBody;
+    public int standBackHash;
+    public int standFrontHash;
     public Transform transform;
-    public float speed = 50.0F;
-    public bool canMove = true;
 
     // Use this for initialization
     void Start()
@@ -19,34 +19,23 @@ public class PlayerController : MonoBehaviour
         Debug.Log("I am alive and my name is " + myName);
         anim = GetComponent<Animator>();
         transform = GetComponent<Transform>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        speed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(canMove){
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-            transform.position += move * speed;
-        }
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
 
-        /*if (move.x > 0)
+        if (move.y < 0)
         {
-             anim.SetTrigger(standFrontHash);
-         }
-        else if (move.x < 0)
-         {
-             anim.SetTrigger(standBackHash);
-         }*/
+            anim.CrossFade(Animator.StringToHash("Stand_Front"), 0f);
+        }
+        else if (move.y > 0)
+        {
+            anim.CrossFade(Animator.StringToHash("Stand_Back"), 0f);
+        }
     }
-
-    /*void OnCollisionEnter2D(Collision2D coll)
-    {
-        canMove = false;
-    }
-    void OnCollisionExit2D(Collision2D coll)
-    {
-        canMove = true;
-
-    }*/
 }
