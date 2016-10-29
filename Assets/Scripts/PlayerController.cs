28fb07110df6dfe9439833f27c504a9f2254f53a
Joffrey Davaïc <joffrey.davaic@gmail.com>
2016-10-28 20:47:28 -0400

    animation wolf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,8 @@ public class PlayerController : MonoBehaviour
     public int standBackHash;
     public int standFrontHash;
     public Transform transform;
-
+    public bool right;
+    public bool left;
     // Use this for initialization
     void Start()
     {
@@ -21,6 +22,9 @@ public class PlayerController : MonoBehaviour
         transform = GetComponent<Transform>();
         rigidBody = GetComponent<Rigidbody2D>();
         speed = 5f;
+        left = true;
+        right = false;
+        isFront = true;
     }
 
     // Update is called once per frame
@@ -29,13 +33,59 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * speed * Time.deltaTime;
 
+        AnimateDirection(move);
+    }
+
+    void AnimateDirection(Vector3 move)
+    {
         if (move.y < 0)
         {
-            anim.CrossFade(Animator.StringToHash("Stand_Front"), 0f);
+            isFront = true;
+            AnimateRun(move);
         }
-        else if (move.y > 0)
+        if (move.y > 0)
         {
-            anim.CrossFade(Animator.StringToHash("Stand_Back"), 0f);
+            isFront = false;
+            AnimateRun(move);
+        }
+        if (move.x > 0)
+        {
+            AnimateRun(move);
+            if (left)
+            {
+                anim.transform.Rotate(new Vector3(0, 1, 0), 180);
+                right = true;
+                left = false;
+            }
+        }
+        if(move.x < 0){
+            AnimateRun(move);
+            if(right){
+                anim.transform.Rotate(new Vector3(0, 1, 0), 180);
+                right = false;
+                left = true;
+            }
+        }
+        if(move.x == 0 && move.y == 0){
+            if(isFront){
+                anim.CrossFade(Animator.StringToHash("Stand_Front"), 0f);
+            }
+            else
+            {
+                anim.CrossFade(Animator.StringToHash("Stand_Back"), 0f);
+            }
+        }
+    }
+
+    void AnimateRun(Vector3 move)
+    {
+        if (isFront)
+        {
+            anim.CrossFade(Animator.StringToHash("Run_Front"), 0f);
+        }
+        else
+        {
+            anim.CrossFade(Animator.StringToHash("Run_Back"), 0f);
         }
     }
 }
